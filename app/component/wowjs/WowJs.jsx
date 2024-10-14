615357dcbefd6ae3a9265c872f2640a953aca9db
AhmedMagdyAbdAlhady <140403024+AhmedMagdyAbdAlhady@users.noreply.github.com>
2024-10-14 13:53:31 +0300

    repair build in the vercel

---
+++ app/component/wowjs/WowJs.jsx
@@ -0,0 +1,15 @@
+'use client'
+import  { useEffect } from 'react';
+import WOW from '../../../node_modules/wowjs/dist/wow.min.js';
+
+
+const WowJs = () => {
+    useEffect(() => {
+        const wow = new WOW.WOW();
+        wow.init();
+    }, []);
+    return null;
+}
+
+export default WowJs;
+
